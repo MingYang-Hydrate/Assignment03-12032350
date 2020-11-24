400c30f09f6fb0aec542285f76996634723b45e4
MingYang-Hydrate <71815635+MingYang-Hydrate@users.noreply.github.com>
2020-11-24 23:44:39 +0800

    Update PS3_7.R
    good work

--- PS3_7.R
+++ PS3_7.R
@@ -8,16 +8,16 @@ library(dplyr)
 library(ggplot2)
 
 #7.1
-#�ֱ�ȡ��������ĳ�غ�������ĳ�ص�ǳ�����ˮˮ������ͬ��ʵ�������£����ˮ���е�CaCo3�������Դ���t���顣
+#分别取自龙华区某地和龙岗区某地的浅层地下水水样，在同等实验条件下，测得水样中的CaCo3含量，以此做t检验。
 LONGGANG <- c(20.99,20.41,20.10,20.00,20.91,22.60,20.99,20.42,20.90,22.99,23.12,20.89)
 LONGHUA  <- c(20.78,21.36,20.10,23.06,21.50,20.81,22.42,20.48,22.90,20.13,20.26,21.86)
 t.test(LONGGANG,LONGHUA,paired=T)
 
 #7.2
-#�����о���ˮ��������Ӱ��̶���Ҫȡ����ˮ�������������Լ�ˮ�����״����Χ���εȡ�
-#һ��˵����ˮ�����������Խ�󣬶���Χ���������Ӱ��ҲԽ��
-#ˮ�������Χ�����Ӱ�죬�漰���¡��硢�¶ȵ�����Ҫ�غͽ�ˮ��������������
-#����ͨ�������������ɻ���Storm Lake��Զ��ɽ�ص���Sioux Rapids4E�Ľ��������ݣ��ó�ˮ��Խ������Ƿ���Ӱ�죬Ӱ����
+#根据研究，水体对气候的影响程度主要取决于水体面积、体积，以及水体的形状和周围地形等。
+#一般说来，水体的面积和体积越大，对周围地区气候的影响也越大。
+#水体对其周围气候的影响，涉及气温、风、温度等气象要素和降水、雾等天气现象。
+#本题通过分析美国爱荷华州Storm Lake和远湖山地地区Sioux Rapids4E的降雨量数据，得出水体对降雨量是否有影响，影响多大
 
 # Load data
 STORM_LAKE_P_value    <-  read.csv("STORM LAKE.csv",header=T)
@@ -52,7 +52,7 @@ bartlett.test(PRCP ~ NAME, data = DATA)
 anova_one_way <- aov(PRCP ~ NAME, data = DATA)
 summary(anova_one_way)
 
-#7.3  �о�CAS city �Ľ������ͺӵ������Ĺ�ϵ�����м򵥵����Իع�
+#7.3  研究CAS city 的降雨量和河道流量的关系，进行简单地线性回归
 # Load data
 Discharge_data      <-  read.csv("discharge.csv",header=T)
 Discharge_Data      <-  as_tibble(Discharge_data)
@@ -67,4 +67,4 @@ model_step_f   <- step(fit,scope=list(lower=fit0,upper=fit),direction='forward')
 summary(model_step_f)
 abline(model_step_f, col="blue")
 
-
+# good work 
